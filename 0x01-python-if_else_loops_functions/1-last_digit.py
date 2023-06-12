b6e3fede256f4b0a563d8853300fa559d76f22c7
Reliccode <bryanmaritim@gmail.com>
2023-06-12 14:41:17 +0000

    fix: Included if & else to capture a -ve sign in the last digit of a negative number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number >= 0:
-    l_digit = number % 10
-else:
-    l_digit = number % -10
-lastDigit = int(repr(number)[-1])
+lastDigit = int(repr(number)[-1]) if number >= 0 else -int(str(-number)[-1])
 print("Last digit of {} is {}".format(number, lastDigit), end='')
 if lastDigit > 5:
     print(" and is greater than 5")
