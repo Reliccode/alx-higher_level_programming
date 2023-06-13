421cb83df630b7769b41aa67ae4866a676f395b7
Reliccode <bryanmaritim@gmail.com>
2023-06-13 05:23:03 +0000

    Print numbers in double digit from 0 to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 mumber = (0, 100)
 for number in range(0, 100):
-    print("{:02}".format(number), end='')
-    if number != 99:
-        print(", ", end='')
+    print("{:02}".format(number), end=', ' if number != 99 else '\n')
