951ec2798a9e8e40d2b8b77ddd5dc257ae177f36
Reliccode <bryanmaritim@gmail.com>
2023-06-14 09:27:44 +0000

    Printing ASCII alphabet in rev order but alt UPPER and LOWERCASE, eg. aBcDeF

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,17 +1,3 @@
 #!/usr/bin/python3
-'''
-looks for numbers in range 97,122 and reverses order
-of sequence using the reversed function.
-The range 97, 123 contains ASCII code values
-I defined a variable 'a' which will take the values
-from the range in reverse order
-'''
 for a in reversed(range(97, 123)):
-    '''
-    printing the numbers in range in character form
-    the 'a' I defined will be printed if it is even, if not,
-    it means it is odd, thus subtracts 32 from the value
-    Subtracting from 32 changes lowercase ASCII values
-    to their corresponding uppercase values
-    '''
     print("{:c}".format(a if (a % 2 == 0) else (a - 32)), end='')
