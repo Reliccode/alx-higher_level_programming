2bb26daecdef27343926d8f8bc8825cdaa6f06a2
Reliccode <bryanmaritim@gmail.com>
2023-06-14 08:11:51 +0000

    fix: Adding comments and rearranging the order of if, elif conditionals

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 def fizzbuzz():
+    # Looking for numbers between 1 and 100
     for numbers in range(1, 101):
-        if numbers % 3 == 0:
+        # when the number is a multiple of both 3 and 5, print FizzBuzz
+        # the modulus checks for a 0 reminder confirming multiplicity
+        if numbers % 3 == 0 and numbers % 5 == 0:
+            print("FizzBuzz ", end='')
+        # When the number is a multiple of 3, print Fizz
+        elif numbers % 3 == 0:
             print("Fizz ", end='')
+        # when the num is a multiple of 5, print Buzz
         elif numbers % 5 == 0:
             print("Buzz ", end='')
-        elif numbers % 3 == 0 and numbers % 5 == 0:
-            print("FizzBuzz ", end='')
+
         else:
             print("{} ".format(numbers), end='')
