4d26ce56e73fd9b731981f030a416764ff5436dc
Reliccode <bryanmaritim@gmail.com>
2023-06-14 07:59:19 +0000

    Trying new code

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,9 +5,7 @@ def fizzbuzz():
             print("Fizz ", end='')
         elif numbers % 5 == 0:
             print("Buzz ", end='')
-        if numbers % 3 == 0 and numbers % 5 == 0:
+        elif numbers % 3 == 0 and numbers % 5 == 0:
             print("FizzBuzz ", end='')
         else:
             print("{} ".format(numbers), end='')
-
-    print()
