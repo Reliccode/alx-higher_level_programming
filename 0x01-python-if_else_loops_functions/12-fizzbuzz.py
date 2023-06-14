466ae36f0f4fc7b38ecf374e8559325396584a0a
Reliccode <bryanmaritim@gmail.com>
2023-06-14 07:24:43 +0000

    fix: Using conditionals to make exclude numbers divisible by 3 and 5
    clear

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,10 +2,10 @@
 def fizzbuzz():
     for numbers in range(1, 101):
         if numbers % 3 == 0:
-            print("Fizz")
-            if numbers % 5 == 0:
-                print("Buzz")
-                if numbers % 5 and 3 == 0:
-                    print("FizzBuzz")
+            print("Fizz", end='')
+        elif numbers % 5 == 0:
+            print("Buzz", end='')
+        elif numbers % 5 == 0 and numbers % 3 == 0:
+            print("FizzBuzz", end='')
         else:
             print("{} ".format(numbers), end='')
