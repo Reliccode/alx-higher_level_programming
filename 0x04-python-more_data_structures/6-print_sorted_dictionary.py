b9fee6d971daa85d681d70ff824fbc1e1fba7fac
Reliccode <bryanmaritim@gmail.com>
2023-06-16 15:56:10 +0000

    Function prints dictionary by ordered keys

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    l_ord = list(a_dictionary.keys())
+    l_ord.sort()
+    for i in l_ord:
+        print("{}: {}".format(i, a_dictionary.get(i)))
