9cb80ecbcaaeb2aaec80804d8b691a61f17b8ede
Reliccode <bryanmaritim@gmail.com>
2023-06-30 10:54:57 +0000

    Class square that defines a square by: (based on 2-square.py)

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""
+Module - Square
+"""
+
+
+class Square:
+    """
+    Class defining a square
+    """
+    def __init__(self, size=0):
+        """
+        Initializes a square
+
+        Args:
+            size: The int size of the square (default: 0)
+        """
+        self.__size = 0
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        Getter method for size attribute
+
+        Returns:
+            int: The int size of the square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter method for size attribute
+
+        Args:
+            value: The int size value to set
+
+        Raises:
+            TypeError: If value is not an integer
+            ValueError: If value is less than 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        Calculates area of square
+
+        Returns:
+            int: The int area of the square
+        """
+        return self.__size ** 2
