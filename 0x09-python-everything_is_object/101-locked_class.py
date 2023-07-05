48878de5fdc79d733309cfcce96108a9395de8c8
Reliccode <bryanmaritim@gmail.com>
2023-07-05 20:56:30 +0000

    A class LockedClass with no class or object attribute, hat prevents the user from dynamically creating new instance attributes, except if the new instance attribute is called first_name

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+class LockedClass:
+    """
+    LockedClass with no attribute that prevents dynamic
+    creation of instance attributes
+    except for 'first_name'.
+    """
+    __slots__ = ['first_name']
