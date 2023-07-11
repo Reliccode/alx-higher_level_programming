97cf22289b195202cbb9c6d7c1a8f3e3bea5e869
Reliccode <bryanmaritim@gmail.com>
2023-07-11 19:04:24 +0000

    function that writes an Object to a text file, using a JSON representation

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,37 +1,21 @@
 #!/usr/bin/python3
+""" Module that writes an Object to a text file using
+a JSON representation
 """
-Module that contains function that writes an 
-Object to a text file using a JSON representation
-"""
-
 import json
 
 
 def save_to_json_file(my_obj, filename):
-    """
-    Writes an Object to a text file using a JSON representation.
+    """ Function that writes an object to a text file
+    by a JSON representation
 
     Args:
-        my_obj: The object to be saved.
-        filename (str): The name of the file to write to.
+        my_obj: object
+        filename: textfile name
+
+    Raises:
+        Exception: when the object can't be encoded
 
     """
-    with open(filename, mode='w', encoding='utf-8') as file:
-        json.dump(my_obj, file)
-
-
-if __name__ == "__main__":
-    my_list = [1, 2, 3]
-    save_to_json_file(my_list, "my_list.json")
-
-    my_dict = {
-        'id': 12,
-        'name': "John",
-        'places': ["San Francisco", "Tokyo"],
-        'is_active': True,
-        'info': {
-            'age': 36,
-            'average': 3.14
-        }
-    }
-    save_to_json_file(my_dict, "my_dict.json")
+    with open(filename, 'w', encoding="utf-8") as f:
+        json.dump(my_obj, f)
