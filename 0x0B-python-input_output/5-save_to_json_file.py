47df243f1064c35053d10118e87e20e045714a64
Reliccode <bryanmaritim@gmail.com>
2023-07-11 18:39:07 +0000

    Trying new code

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Module contains function that writes an object to a text file 
-using a JSON representation
+Module that contains function that writes an 
+Object to a text file using a JSON representation
 """
 
 import json
@@ -9,7 +9,7 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """
-    Writes an object to a text file using a JSON representation.
+    Writes an Object to a text file using a JSON representation.
 
     Args:
         my_obj: The object to be saved.
