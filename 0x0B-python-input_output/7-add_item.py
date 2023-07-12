02afd093c9e2ba451e1c16b88f2303538da087e9
Reliccode <bryanmaritim@gmail.com>
2023-07-12 10:56:17 +0000

    script that adds all arguments to a Python list, and then save them to a file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,24 +4,25 @@ Adds all arguments to a Python list and saves them to a file
 """
 
 import sys
-from os.path import exists
 from save_to_json_file import save_to_json_file
 from load_from_json_file import load_from_json_file
 
 
-def add_item(args: List[str]) -> None:
+def add_item(args):
     """
     Adds all arguments to a Python list and saves them to a file.
 
     Args:
-        args (List[str]): The list of arguments.
+        args: The list of arguments.
 
     """
     filename = "add_item.json"
     my_list = []
 
-    if exists(filename):
+    try:
         my_list = load_from_json_file(filename)
+    except FileNotFoundError:
+        pass
 
     my_list.extend(args)
     save_to_json_file(my_list, filename)
