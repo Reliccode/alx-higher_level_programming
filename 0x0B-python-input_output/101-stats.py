57b22282cdd48fd1fd89400255d155b3325d91b8
Reliccode <bryanmaritim@gmail.com>
2023-07-12 13:44:50 +0000

    fix: Modifyin exception to print error message and continue execution

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -41,20 +41,15 @@ if __name__ == "__main__":
 
     try:
         for line in sys.stdin:
-            if nlines % 10 == 0 and nlines != 0:
+            if nlines % 10 == 0 and nlines is not 0:
                 magic.print_info()
 
             try:
                 list_line = [x for x in line.split(" ") if x.strip()]
-                if len(list_line) >= 2:
-                    magic.add_status_code(list_line[-2])
-                if len(list_line) >= 3:
-                    try:
-                        magic.size += int(list_line[-1].strip("\n"))
-                    except ValueError:
-                        print(f"Invalid file size: {list_line[-1].strip()}")
-            except Exception as e:
-                print(f"An error occurred: {str(e)}")
+                magic.add_status_code(list_line[-2])
+                magic.size += int(list_line[-1].strip("\n"))
+            except TypeError:
+                pass
             nlines += 1
     except KeyboardInterrupt:
         magic.print_info()
