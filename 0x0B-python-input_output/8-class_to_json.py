0599fb3dd4de4af7b77a1dfb3eac3935356cc52d
Reliccode <bryanmaritim@gmail.com>
2023-07-12 13:12:05 +0000

    function that returns the dictionary description with simple data structure (list, dictionary, string, integer and boolean) for JSON serialization of an object

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module with a function that contains the function class_to_json
+"""
+
+def class_to_json(obj):
+    """
+    Returns the dictionary description of an object for JSON serialization.
+
+    Args:
+        obj: An instance of a Class.
+
+    Returns:
+        A dictionary representation of the object.
+
+    """
+    return obj.__dict__
