83351b56c2b42908f1571d129927046ab2ba99e6
Reliccode <bryanmaritim@gmail.com>
2023-07-12 14:14:49 +0000

    Piped the output of code to generator

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,57 +1,40 @@
 #!/usr/bin/python3
-""" Module to print status code """
+"""Module to print status code"""
 import sys
 
+lines_processed = 0
+total_file_size = 0
+status_code_counts = {}
 
-class Magic:
-    """ Class to generates instances with dict and size"""
-    def __init__(self):
-        """ Init method """
-        self.dic = {}
-        self.size = 0
+try:
+    for line in sys.stdin:
+        lines_processed += 1
 
-    def init_dic(self):
-        """ Initialize dict """
-        self.dic['200'] = 0
-        self.dic['301'] = 0
-        self.dic['400'] = 0
-        self.dic['401'] = 0
-        self.dic['403'] = 0
-        self.dic['404'] = 0
-        self.dic['405'] = 0
-        self.dic['500'] = 0
+        # Extract relevant information from the line
+        parts = line.split()
+        if len(parts) >= 7:
+            status_code = parts[-2]
+            file_size = int(parts[-1])
 
-    def add_status_code(self, status):
-        """ add repeated number to the status code """
-        if status in self.dic:
-            self.dic[status] += 1
+            # Update the total file size
+            total_file_size += file_size
 
-    def print_info(self, sig=0, frame=0):
-        """ print status code """
-        print("File size: {:d}".format(self.size))
-        for key in sorted(self.dic.keys()):
-            if self.dic[key] is not 0:
-                print("{}: {:d}".format(key, self.dic[key]))
+            # Update the status code count
+            if status_code in status_code_counts:
+                status_code_counts[status_code] += 1
+            else:
+                status_code_counts[status_code] = 1
 
+        # Print statistics every 10 lines
+        if lines_processed % 10 == 0:
+            print("File size:", total_file_size)
+            for code in sorted(status_code_counts):
+                count = status_code_counts[code]
+                print(code + ":", count)
 
-if __name__ == "__main__":
-    magic = Magic()
-    magic.init_dic()
-    nlines = 0
-
-    try:
-        for line in sys.stdin:
-            if nlines % 10 == 0 and nlines is not 0:
-                magic.print_info()
-
-            try:
-                list_line = [x for x in line.split(" ") if x.strip()]
-                magic.add_status_code(list_line[-2])
-                magic.size += int(list_line[-1].strip("\n"))
-            except:
-                pass
-            nlines += 1
-    except KeyboardInterrupt:
-        magic.print_info()
-        raise
-    magic.print_info()
+except KeyboardInterrupt:
+    # Print statistics when a keyboard interruption occurs
+    print("File size:", total_file_size)
+    for code in sorted(status_code_counts):
+        count = status_code_counts[code]
+        print(code + ":", count)
