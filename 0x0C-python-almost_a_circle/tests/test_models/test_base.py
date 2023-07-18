fd8c7bb7805ee934b65dc26fa46942802880d559
Reliccode <bryanmaritim@gmail.com>
2023-07-18 15:07:30 +0000

    fix: Added import os to resolve NameErroe when trying to use os.remove()

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -7,6 +7,7 @@ from models.rectangle import Rectangle
 from io import StringIO
 from unittest import TestCase
 from unittest.mock import patch
+import os
 
 
 class TestBaseMethods(unittest.TestCase):
