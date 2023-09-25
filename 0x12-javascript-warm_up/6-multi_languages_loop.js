7fe48f13501bb08266a5983119f61eef94c090e8
Reliccode <bryanmaritim@gmail.com>
2023-09-25 12:15:00 +0300

    Loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 
-const msg = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < 3; i++) {
-  console.log(msg[i]);
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (const line of lines) {
+  console.log(line);
 }
