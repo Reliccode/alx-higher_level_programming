3ac8217270355960f0a03a618a25a88e2b5b6d79
Reliccode <bryanmaritim@gmail.com>
2023-09-28 17:17:25 +0000

    Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # This script sends a PUT request to 0.0.0.0:5000/catch_me to get the desired response
 
-# Send a PUT request with the specified data
-curl -sLX PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin: HolbertonSchool" -o /dev/null -w "You got me!"
+# Send a PUT request with the specified data and headers
+curl -s -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me | grep "You got me!" --quiet && echo "You got me!"
