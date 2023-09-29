46fe6a2b99e3bb816de126f39b00c8bd97cec463
Reliccode <bryanmaritim@gmail.com>
2023-09-29 17:50:13 +0000

    fix: Removing exception

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -8,8 +8,10 @@ import sys
 
 
 if __name__ == "__main__":
-    try:
-        r = requests.get(sys.argv[1])
-        print(r.headers['X-Request-Id'])
-    except Exception as e:
-        print(f"An error occured : {e}")
+    url = sys.argv[1]
+    response = requests.get(url)
+
+    x_r_id = response.headers.get('X-Request-Id')
+
+    if x_r_id is not None:
+        print(x_r_id)
