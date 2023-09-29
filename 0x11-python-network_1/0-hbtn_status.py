aced0b860b000ca176136298ab24c780c32ba3af
Reliccode <bryanmaritim@gmail.com>
2023-09-29 16:48:43 +0000

    fix: Encapsulated code within a function

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,24 @@
 #!/usr/bin/python3
-""" Python script that fetches https://alx-intranet.hbtn.io/status """
+"""Fetches https://alx-intranet.hbtn.io/status using urllib."""
+
 import urllib.request
 
+def fetch_and_display_status():
+    """
+    Fetches the status from https://alx-intranet.hbtn.io/status
+    and displays info.
+    """
+    url = 'https://alx-intranet.hbtn.io/status'
 
-if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        html = response.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(html)))
-        print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(html.decode("utf-8")))
+    try:
+        with urllib.request.urlopen(url) as response:
+            html = response.read()
+            print('Body response:')
+            print('\t- type: {}'.format(type(html)))
+            print('\t- content: {}'.format(html))
+            print('\t- utf8 content: {}'.format(html.decode('utf-8')))
+    except urllib.error.URLError as e:
+        print('Error:', e)
+
+if __name__ == '__main__':
+    fetch_and_display_status()
