52b2a4c09a6db78d47e673b95649e6cb7881c819
Reliccode <bryanmaritim@gmail.com>
2023-09-29 17:33:21 +0000

    script that takes in a URL, sends a request to the URL and displays the value of the variable X-Request-Id in the response header

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+script that sends request to URL and
+displays value of a variable in response header
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    try:
+        r = requests.get(sys.argv[1])
+        print(r.headers['X-Request-Id'])
+    except:
+        pass
