a45dbc8a516e3e3723d1eb65a826dbb598576a5f
Reliccode <bryanmaritim@gmail.com>
2023-10-24 22:58:07 +0300

    [Refactor] Removing reduntant variable assignment

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,20 +1,30 @@
 #!/usr/bin/node
 const request = require('request');
 
-const movieId = parseInt(process.argv[2], 10);
+const movieId = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
 request(url, (error, response, body) => {
-  if (!error) {
+  if (!error && response.statusCode === 200) {
     const film = JSON.parse(body);
     const characters = film.characters;
 
-    characters.forEach((characterUrl) => {
-      request(characterUrl, (err, response, charBody) => {
-        if (!err) {
-          console.log(JSON.parse(charBody).name);
-        }
-      });
-    });
+    function fetchCharacter (index) {
+      if (index < characters.length) {
+        request(characters[index], (err, charResponse, charBody) => {
+          if (!err && charResponse.statusCode === 200) {
+            const character = JSON.parse(charBody);
+            console.log(character.name);
+            fetchCharacter(index + 1);
+          } else {
+            console.log('Error fetching character data');
+          }
+        });
+      }
+    }
+
+    fetchCharacter(0);
+  } else {
+    console.log('Error fetching film data');
   }
 });
