ab736483234dcdfce4eb378cd0146b529127b687
Reliccode <bryanmaritim@gmail.com>
2023-11-02 19:14:08 +0300

    script that fetches from https://hellosalut.stefanbohacek.dev/?lang=fr and displays the value of hello from that fetch in the HTML tag DIV#hello.

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,4 +1,4 @@
 const $ = window.$;
-$.get('https://fourtonfish.com/hellosalut/?lang=fr', function (data, textStatus) {
+$.get('https://hellosalut.stefanbohacek.dev/?lang=fr', function (data, textStatus) {
   $('DIV#hello').text(data.hello);
 });
